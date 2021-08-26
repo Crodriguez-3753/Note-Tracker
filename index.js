6e04fab9410e99bf0720bc2d128ad5ff3a098dcf
Carlos Rodriguez <crodriguez3753@gmail.com>
2021-08-25 20:17:30 -0400

    post route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const notesJSON = require("./db/db.json");
 
 //const notesArray = JSON.parse (notesJSON);
@@ -6,20 +7,30 @@ console.log(notesJSON);
 const app = express();
 const PORT = 3000;
 
+//MIDDLEWARE
+app.use(express.json())
+//url encoded middleware
+app.use(express.urlencoded())
 
 // server static 
 app.use(express.static("public"));
 
-//json middleware
-//url encoded middleware
 
-//API Routes// (save/rewrite and load)
+//API ROUTES// (save/rewrite and load)
+
+// get API notes
 app.get("/api/notes", (req, res) => {
-    console.log("/api/notes");
     res.json(notesJSON)
 });
-// get API notes
-//post API notes
 
+//post API notes
+app.post("/api/notes", (req, res) => {
+   notesJSON.push(req.body)
+   res.json(notesJSON.length -1)
+})
+
+// HTML routes
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+app.get("*", (req, res) => res.send("404"));
 //start listening
 app.listen(PORT, ()=> console.log("server running on http://localhost:3000"));
